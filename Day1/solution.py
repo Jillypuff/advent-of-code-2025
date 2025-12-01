6f0f37dfb3930a8970644882ffe5b73ceacb63ec
Jesper Lindberg <Jillypuff@protonmail.com>
2025-12-01 16:13:10 +0100

    Day 1

---
+++ Day1/solution.py
@@ -0,0 +1,30 @@
+def turn_dial(position, line, clicks):
+    direction, rotations = split_line(line)
+    while rotations > 99:
+        clicks += 1
+        rotations -= 100
+
+    if direction == "R":
+        if position + rotations > 99:
+            clicks += 1
+        position += rotations
+    else:
+        if position != 0 and position - rotations <= 0:
+            clicks += 1
+        position -= rotations
+
+    return position % 100, clicks
+
+def split_line(line):
+    return line[0], int(line[1:])
+
+with open("Day1/data.txt", "r") as file:
+    lines = file.read().splitlines()
+
+position = 50
+clicks = 0
+
+for line in lines:
+    position, clicks = turn_dial(position, line, clicks)
+
+print(clicks)
